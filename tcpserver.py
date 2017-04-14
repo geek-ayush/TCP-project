0be874db58ca4a9fe92721b114e14c7884676d2c
Ayush Yadav <geek-ayush@users.noreply.github.com>
2017-04-14 23:02:45 +0530

    Add files via upload

--- tcpserver.py
+++ tcpserver.py
@@ -1,44 +1,53 @@
 import socket
 from datetime import datetime
 
+
 class Server():
     def __init__(self):
-        self.Socket_Object = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.Socket_Object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.HostName = socket.gethostname()
         self.Port = 12345
-        self.Socket_Object.bind((self.HostName,self.Port))
+        self.Socket_Object.bind((self.HostName, self.Port))
 
-    def Start(self): 
-        self.Socket_Object.listen(5) #at max 5 connections
-        print("Server Started at :  #{}".format(datetime.now()))
+    def Start(self):
+        self.Socket_Object.listen(5)   # at max 5 connections
+        print("Server Started at :#{}".format(datetime.now()))
+
     def Close(self):
-        self.Socket_Object.shutdown(2)       # 0 : recieves are diallowed , 1: sends are disallowed , 2: both are disallowed
+        self.Socket_Object.shutdown(2)  # 0:disallowRecv,1:disallowSend,2:both
         self.Socket_Object.close()
-        print("Server Closed at :   #{}".format(datetime.now()))
+        print("Server Closed at :#{}".format(datetime.now()))
+
 
 class Server_Connection():
     def __init__(self):
-            self.buffersize = 128 #1024 commonly used but for faster response
-            self.conn = None
-    def Start_Connection(self,server):
+        self.buffersize = 128   # 1024 commonly used but for faster response
+
+    def Start_Connection(self, Server):
         try:
             self.time_start = datetime.now()
-            while True:
-                self.conn , self.Address = Server.Socket_Object.accept()
-                self.conn.data_received ,self.conn.ip = self.conn.recvfrom()
-                print("\n Recieved data : ",self.conn.data_received)
-                if self.conn.data_received.upper() != ("Q" or "EXIT") :
-                    self.conn.send("Connection Closed Successfully !!")
-                    self.conn.close()
-                    self.sendto(32,self.conn.ip)
+            self.conn, self.Address = Server.Socket_Object.accept()
+            print("Server Connected to :", self.Address)
+            data_received = b''
+            while True:                
+                data_received = self.conn.recv(128)
+                self.data_received = data_received.decode('ascii')
+                print("\n Recieved data : #{} at #{}".format(self.data_received,datetime.now()))
+                if self.data_received != (b"Q" or b"EXIT"):
+                    self.conn.send(b"Connection Closed Successfully !!")
+                    self.Close_Connection()
+                self.send(b"Server Recieved mssg")
         except:
-            self.conn.close()
-            sel.time_end = datetime.now() 
+            self.Close_Connection()
+            self.time_end = datetime.now()
+
 
     def Close_Connection(self):
         self.conn.close()
+        print("Server Connection has been Closed.")
+
 
 s = Server()
 s.Start()
-s.Close()
-
+st = Server_Connection()
+st.Start_Connection(s)
