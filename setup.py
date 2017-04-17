a0cc4ca092e6ceafcbff5d0d8fe66b09ea3a21d6
Ayush Yadav <geek-ayush@users.noreply.github.com>
2017-04-17 11:14:33 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup( name = "tcpserver",
+        version = "1.0.0",
+        description = " Python 3.x based TCP SERVER",
+        author = "Ayush",
+        scripts = ["bin/tcpserver"],
+        packages = ["src"])
